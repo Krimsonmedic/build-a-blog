dc54d54db01675acd359577a439b6e6652fe7777
Matt Bland <krimsoninfinity@gmail.com>
2019-05-08 19:37:47 -0500

    final draft

--- main.py
+++ main.py
@@ -3,29 +3,63 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:getitdone@localhost:3306/get-it-done'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildit@localhost:3306/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-class Task(db.Model):
+class Blog(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+    
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        
+        
 
-tasks = []
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        blog_title = request.form['blogname']
+        blog_body = request.form['body']
+        blog_post=Blog(blog_title, blog_body)
+        
+        db.session.add(blog_post)
+        db.session.commit()
+    
+    
+    bloglist = Blog.query.all()
+    
+    
+    return render_template('todos.html',title="Bloggy Fresh!", bloglist=bloglist )
 
-    return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
+@app.route('/bloglists', methods=['GET'])
+def bloglisting():
+    bloglist = Blog.query.all()
+    return render_template('bloglists.html', bloglist=bloglist)
+
+@app.route('/posts', methods=['GET'])
+def bloglink():
+    blog_id= request.args.get('id')
+    print(blog_id)
+    blog= get_blog(blog_id)
+    return render_template('posts.html', blog = blog)
+    
+
+def get_blog(id):
+    print(id)
+    return Blog.query.get(id)
+    
+
+
+
+   
 if __name__ == '__main__':
     app.run()
